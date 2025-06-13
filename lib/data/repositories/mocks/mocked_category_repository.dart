fac995a7f4a307a1fe60e7437ac5a2c213d5cafa
AskarKasimov <kasimovaskar123@gmail.com>
2025-06-13 15:52:37 +0300

    feat: all repositories have been mocked in RAM with simple methods

---
+++ lib/data/repositories/mocks/mocked_category_repository.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_finances/domain/entities/category.dart';
+import 'package:flutter_finances/domain/failures/failure.dart';
+import 'package:flutter_finances/domain/failures/repository_failure.dart';
+import 'package:flutter_finances/domain/repositories/category_repository.dart';
+
+class MockedCategoryRepository implements CategoryRepository {
+  final List<Category> _mockCategories = [
+    Category(id: 1, name: 'Зарплата', isIncome: true, emoji: ''),
+    Category(id: 2, name: 'Подарок', isIncome: true, emoji: ''),
+    Category(id: 3, name: 'Еда', isIncome: false, emoji: ''),
+    Category(id: 4, name: 'Развлечения', isIncome: false, emoji: ''),
+  ];
+
+  @override
+  Future<Either<Failure, List<Category>>> getAllCategories() async {
+    try {
+      return right(_mockCategories);
+    } catch (e) {
+      return left(RepositoryFailure('Ошибка при получении категорий'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Category>>> getCategoriesByIsIncome(
+    bool isIncome,
+  ) async {
+    final filtered =
+        _mockCategories.where((c) => c.isIncome == isIncome).toList();
+    return right(filtered);
+  }
+}
