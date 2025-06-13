24a0149650a5d9c81dddd4730b53a3713852f94d
AskarKasimov <kasimovaskar123@gmail.com>
2025-06-13 15:27:57 +0300

    feat: replaced usual Future<> with Either<> inside

--- lib/domain/repositories/category_repository.dart
+++ lib/domain/repositories/category_repository.dart
@@ -1,7 +1,11 @@
-import '../entities/category.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_finances/domain/entities/category.dart';
+import 'package:flutter_finances/domain/failures/failure.dart';
 
 abstract interface class CategoryRepository {
-  Future<List<Category>> getAllCategories();
+  Future<Either<Failure, List<Category>>> getAllCategories();
 
-  Future<List<Category>> getCategoriesByIsIncome(bool isIncome);
+  Future<Either<Failure, List<Category>>> getCategoriesByIsIncome(
+    bool isIncome,
+  );
 }
--- lib/domain/repositories/transaction_repository.dart
+++ lib/domain/repositories/transaction_repository.dart
@@ -1,16 +1,23 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_finances/domain/entities/forms/transaction_form.dart';
 import 'package:flutter_finances/domain/entities/transaction.dart';
+import 'package:flutter_finances/domain/failures/failure.dart';
 
 abstract interface class TransactionRepository {
-  Future<Transaction> createTransaction(TransactionForm transaction);
+  Future<Either<Failure, Transaction>> createTransaction(
+    TransactionForm transaction,
+  );
 
-  Future<Transaction> getTransactionById(int id);
+  Future<Either<Failure, Transaction>> getTransactionById(int id);
 
-  Future<Transaction> updateTransaction(int id, TransactionForm transaction);
+  Future<Either<Failure, Transaction>> updateTransaction(
+    int id,
+    TransactionForm transaction,
+  );
 
-  Future<void> deleteTransaction(int id);
+  Future<Either<Failure, Unit>> deleteTransaction(int id);
 
-  Future<List<Transaction>> getTransactionsByPeriod(
+  Future<Either<Failure, List<Transaction>>> getTransactionsByPeriod(
     int accountId,
     DateTime startDate,
     DateTime endDate,
